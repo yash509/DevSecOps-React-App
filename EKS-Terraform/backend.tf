316b3da3e175aa239f9aca60891983c78d3750e7
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-03-20 11:21:52 +0530

    Update backend.tf

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "backend-react-app" # Replace with your actual S3 bucket name
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
